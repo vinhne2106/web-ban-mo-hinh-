e282aa69e57fde3bccdd3dea6987ec994a1fc254
vinhne2106 <vinhne21062005@gmail.com>
2026-05-21 19:04:00 +0700

    update code

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,17 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand fw-bold text-danger fs-4" href="#!">MECHASHOP</a>
+                <a class="navbar-brand d-flex align-items-center fw-bold text-uppercase" href="#!" style="letter-spacing: 1px;">
+    <svg width="38" height="38" viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" class="me-2">
+        <rect width="44" height="44" rx="8" fill="#F97316"/>
+        <path d="M10 30 L10 18 Q10 10 22 10 Q34 10 34 18 L34 30 Z" fill="#1a1a1a"/>
+        <rect x="10" y="27" width="24" height="5" rx="2" fill="#F97316"/>
+        <rect x="14" y="17" width="6" height="4" rx="1" fill="#F97316"/>
+        <rect x="24" y="17" width="6" height="4" rx="1" fill="#F97316"/>
+        <rect x="18" y="24" width="8" height="2" rx="1" fill="#F97316"/>
+    </svg>
+    <span class="text-dark">MECHA</span><span style="color:#F97316">SHOP</span>
+</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
